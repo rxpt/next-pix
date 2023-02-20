13477fafabcc578231d0b25747a686885804a113
Ronis Xogum <ronis@xogum.net>
2023-02-20 15:23:31 -0300

    Update payment.js

--- src/pages/api/mercadopago/payment.js
+++ src/pages/api/mercadopago/payment.js
@@ -4,7 +4,7 @@ import { getSession } from "next-auth/react";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const session = await getSession({ req });
-    if (!session || session.user.name !== req.body.userName) {
+    if (!session) {
       return res.status(401).json({
         error: "Unauthorized",
       });
